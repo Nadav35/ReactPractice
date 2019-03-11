535036615350062778015d0ac87f576a1f7cdec9
Nadav <nadavnoy@yahoo.com>
2019-03-11 14:51:07 -0700

    widgets updates

--- widgets/src/components/Weather.js
+++ widgets/src/components/Weather.js
@@ -1,10 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Weather extends Component {
+
+  state = {
+    lat: '',
+    lng: '',
+    location: '',
+    temp: ''
+  }
+
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(pos => {
+      // console.log(pos);
+      
+      this.setState({ 
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
+      })
+      
+    })
+  }
+
+  setVars() {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lng}&units=imperial&APPID=f8f928a9a8a90741de8f48b66d244d81`)
+      .then(res => {
+        // console.log(res.data);
+        this.setState({
+          location: res.data.name,
+          temp: res.data.main.temp
+
+        })
+
+      })
+  }
+
+  
+
   render() {
+    if (this.state.lat) {
+      this.setVars();
+    }
+    
+    
+    const { location, temp } = this.state;
     return (
-      <div>
-        yo
+      <div className="weather-container">
+        <h1>{location}  {temp}</h1>
+        <div className="weather-pane">
+
+
+        </div>
       </div>
     )
   }
